bc5b80ef74149eb4719c0608566cde555dd07af6
Darsh Wala <darshwala@gmail.com>
2026-03-15 21:30:56 +0530

    added input and add items button

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import chef from "D:/reactProject/chef-claude/src/IMAGES/robotic.png"
+export default function Header(){
+    return(
+        <>
+        <header id="main-header">
+            <img id="robotchef" src= {chef} alt="robot-chef"/>
+            <h1>Chef Claude</h1>
+        </header>
+
+        </>
+    )
+}
---
+++ src/components/ingredients-addition.jsx
@@ -0,0 +1,8 @@
+export default function input(){
+    return(
+        <section id="addition-of-items">
+            <input className="search-box" ></input>
+            <button className="search-box add-button" >Add Item</button>
+        </section>
+    )
+}
